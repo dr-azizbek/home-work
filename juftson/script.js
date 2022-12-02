0d8807fc12be7667bfa0639603e80057f119de3e
dr-azizbek <115967245+dr-azizbek@users.noreply.github.com>
2022-12-02 08:12:07 +0500

    innerhtml

--- juftson/script.js
+++ juftson/script.js
@@ -2,7 +2,7 @@ let n = +prompt("Raqamni kiriting (juft son hosil qilinadi):")
 let son2 = 2
 let result = son2 * n;
 let P = document.getElementById("juft-son")
-P.innerHTML = "Juft son formulasi:I = 2n + 1 \n Juft son:" + result
+P.innerHTML = "Juft son:" + result
 
 
 
