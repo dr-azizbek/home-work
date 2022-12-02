6e34b19f22981b750289751152be9a72453f3175
dr-azizbek <115967245+dr-azizbek@users.noreply.github.com>
2022-12-02 08:52:52 +0500

    if <=

--- armiya/script.js
+++ armiya/script.js
@@ -5,7 +5,7 @@ let boyKirit = +prompt("Harbiy xizmatga qabul qilish: \n Bo'yingizni kriting:")
 let vaznKirit = +prompt("Vazningizni kiriting:")
 
 let qabul = document.querySelector("h1")
-if (boy <= boyKirit && vazn <= vaznKirit && boy - vazn <= 10) {
+if (boy <= boyKirit && vazn <= vaznKirit && boy - vazn <= 110) {
     qabul.innerText = "Harbiy xizmatga qabul qilinasiz! \n Rahmat!"
 }
 else (
