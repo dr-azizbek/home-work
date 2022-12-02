936e174536cb9fb54d1f968ec6f3ee2e6b8c25cf
dr-azizbek <115967245+dr-azizbek@users.noreply.github.com>
2022-12-02 08:37:36 +0500

    let

--- Kech qolish/script.js
+++ Kech qolish/script.js
@@ -1,13 +1,13 @@
 let vaqtdi = +prompt("Darsga kelgan vaqtingizni kiriting (8.00):")
-let KVaqtdi2 = 8.20
+let KVaqtdi = 8.00
 
 
 let javobi = document.querySelector("#kech__qolish")
-if (KVaqtdi2 >= vaqtdi) {
+if (KVaqtdi >= vaqtdi) {
     javobi.innerHTML = "Darsga o'z vaqtdida keldingiz! \n Rahmat!"
 }
 else {
-    if (KVaqtdi2 < vaqtdi)
+    if (KVaqtdi < vaqtdi)
         javobi.innerHTML = "Darsga kech qoldingiz, adminga uchrashing! \n Rahmat!"
 }
 
